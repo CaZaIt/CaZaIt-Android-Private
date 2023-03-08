63f696f1729896de447a21b6f904770732cc9a54
bmsk <vv99911@gmail.com>
2023-03-09 01:22:50 +0900

    [Add] #9 - MainActivity, CafeListFragment, CafeListViewModel

---
+++ app/src/androidTest/java/org/cafelist/CafeListFragment.kt
@@ -0,0 +1,20 @@
+package org.cafelist
+
+import org.cazait.R
+import org.cazait.databinding.FragmentCafeListBinding
+import org.cazait.ui.base.BaseFragment
+
+class CafeListFragment : BaseFragment<FragmentCafeListBinding, CafeListViewModel>(
+    CafeListViewModel::class.java,
+    R.layout.fragment_cafe_list
+) {
+
+    override fun initView() {
+
+    }
+
+    override fun initAfterBinding() {
+
+    }
+
+}
---
+++ app/src/main/java/org/cazait/ui/component/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.cazait.ui.component
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.cazait.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
