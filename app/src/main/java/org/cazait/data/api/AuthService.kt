f4a4b7f1c054a1a0129220c9e16aaae4e9b2a857
bmsk <vv99911@gmail.com>
2023-03-12 17:30:14 +0900

    [Add] #13 - Auth, User API
    Co-Authored-By: wbham <51990139+WooBinHam@users.noreply.github.com>

---
+++ app/src/main/java/org/cazait/data/api/AuthService.kt
@@ -0,0 +1,23 @@
+package org.cazait.data.api
+
+import org.cazait.data.model.request.SignInReq
+import org.cazait.data.model.response.RefreshTokenRes
+import org.cazait.data.model.response.SignInRes
+import retrofit2.Call
+import retrofit2.http.*
+
+interface AuthService {
+    @POST("/api/auths/log-in")
+    fun postSignIn(
+        @Query("role") role: String = "user",
+        @Body signInReq: SignInReq,
+    ): Call<SignInRes>
+
+    @GET("api/auths/{userId}")
+    fun postRefreshToken(
+        @Path("userId") userId: Long,
+        @Query("role") role: String = "user",
+        // 헤더맵
+    ): Call<RefreshTokenRes>
+
+}
---
+++ app/src/main/java/org/cazait/data/api/UserService.kt
@@ -0,0 +1,4 @@
+package org.cazait.data.api
+
+interface UserService {
+}
