7b29ed7a5a64066d31876db59b597bb851d58e89
Jaeeepp <101336601+Jaeeepp@users.noreply.github.com>
2023-08-17 02:45:34 +0900

    [Refact]#170-약관 세부내용 적용완료
    - 약관 세부내용 적용
    - raw파일 생성
    - 약관이 담긴 txt파일 2개 생성
    - LocationTermsFragment, PrivacyTermsFragment 각각 수정완료

--- app/src/main/java/org/cazait/ui/termspolicies/PrivacyTermsFragment.kt
+++ app/src/main/java/org/cazait/ui/termspolicies/PrivacyTermsFragment.kt
@@ -1,10 +1,15 @@
 package org.cazait.ui.termspolicies
 
+import android.content.Context
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import org.cazait.R
 import org.cazait.databinding.FragmentPrivacyTermsBinding
 import org.cazait.ui.base.BaseFragment
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
 
 @AndroidEntryPoint
 class PrivacyTermsFragment: BaseFragment<FragmentPrivacyTermsBinding, PrivacyTermsViewModel>(
@@ -16,9 +21,32 @@ class PrivacyTermsFragment: BaseFragment<FragmentPrivacyTermsBinding, PrivacyTer
         binding.clTop.btnBack.setOnClickListener {
             findNavController().popBackStack()
         }
+        // termslocation.txt 파일을 읽어와서 txt_location TextView에 설정
+        val termsText = readTextFile(requireContext(), R.raw.termsprivacy)
+        binding.txtPrivacy.text = termsText  // txt_location 아이디의 TextView에 설정
     }
 
     override fun initAfterBinding() {
 
+    }
+    private fun readTextFile(context: Context, resourceId: Int): String {
+        val text = StringBuilder()
+
+        try {
+            val inputStream: InputStream = context.resources.openRawResource(resourceId)
+            val reader = BufferedReader(InputStreamReader(inputStream))
+            var line: String?
+
+            while (reader.readLine().also { line = it } != null) {
+                text.append(line).append('\n')
+            }
+
+            reader.close()
+            inputStream.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return text.toString()
     }
 }
