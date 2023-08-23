657ddad08e8ba948fbc817f72dae86df7ccc05b5
wbham <wbham97@naver.com>
2023-08-23 13:46:18 +0900

    [Feat] #172 - 비밀번호 찾기 화면 구현
    - 비밀번호 EditText에 정규식 적용

--- app/src/main/java/org/cazait/ui/findaccount/findpassword/FindUserPasswordFragment.kt
+++ app/src/main/java/org/cazait/ui/findaccount/findpassword/FindUserPasswordFragment.kt
@@ -1,12 +1,17 @@
 package org.cazait.ui.findaccount.findpassword
 
-import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import org.cazait.R
 import org.cazait.databinding.FragmentFindUserPasswordBinding
 import org.cazait.ui.base.BaseFragment
+import org.cazait.ui.signup.CheckTextWatcher
+import org.cazait.utils.SingleEvent
+import org.cazait.utils.observe
+import org.cazait.utils.showToast
 
 @AndroidEntryPoint
 class FindUserPasswordFragment :
@@ -15,24 +20,158 @@ class FindUserPasswordFragment :
         R.layout.fragment_find_user_password
     ) {
     private val navArgs: FindUserPasswordFragmentArgs by navArgs()
+    private var passwordFlag = false
+    private var passwordCheckFlag = false
     override fun initView() {
         binding.apply {
             clTop.includedTvTitle.text = resources.getString(R.string.btn_find_password)
             clTop.btnBack.setOnClickListener {
                 navigateToSignInFragment()
             }
+            tvUserId.text = navArgs.userId.toString()
         }
-        val userId = navArgs.userId.toString()
-        val userUuid = navArgs.userUuid.toString()
-        Log.d("비밀번호 찾기 정보", userId)
-        Log.d("비밀번호 찾기 정보", userUuid)
+        initEditTextListener()
+        initPatchBtn()
+        observeViewModel()
     }
 
     override fun initAfterBinding() {
 
     }
 
+    private fun observeViewModel() {
+        observeToast(viewModel.showToast)
+    }
+
+    private fun initPatchBtn() {
+        binding.btnFindPasswordJoin.setOnClickListener {
+            val pw = binding.etFindPasswordInsert.text.toString()
+            val repw = binding.etFindPasswordInsertMore.text.toString()
+
+            if (pw == "" || repw == "") {
+                viewModel.showToastMessage(resources.getString(R.string.sign_up_req_all))
+            } else if (pw == repw) {
+                viewModel.showToastMessage(resources.getString(R.string.sign_up_req_suc))
+                // 버튼 클릭시 서버에 비밀번호 변경 요청 보내기
+            } else {
+                viewModel.showToastMessage(resources.getString(R.string.sign_up_req_nopw))
+            }
+        }
+    }
+
     private fun navigateToSignInFragment() {
         findNavController().navigate(FindUserPasswordFragmentDirections.actionFindUserPasswordFragmentToSignInFragment())
+    }
+
+    private fun observeToast(event: LiveData<SingleEvent<Any>>) {
+        binding.root.showToast(this, event, Snackbar.LENGTH_LONG)
+    }
+
+    private fun initEditTextListener() {
+        binding.etFindPasswordInsert.addTextChangedListener(passwordListener)
+        binding.etFindPasswordInsertMore.addTextChangedListener(passwordAgainListener)
+    }
+
+    private val passwordListener = object : CheckTextWatcher() {
+        override fun checkFlag() {
+            binding.btnFindPasswordJoin.isEnabled = passwordFlag && passwordCheckFlag
+        }
+
+        override fun checkText(text: String) = checkPassword(text)
+    }
+
+    private val passwordAgainListener = object : CheckTextWatcher() {
+        override fun checkFlag() {
+            binding.btnFindPasswordJoin.isEnabled = passwordFlag && passwordCheckFlag
+        }
+
+        override fun checkText(text: String) = checkPasswordAgain(text)
+    }
+
+    private fun passwordRegex(password: String): Boolean =
+        password.matches("^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[\$@\$!%*#?&.])[A-Za-z[0-9]\$@\$!%*#?&.]{8,16}\$".toRegex())
+
+    private fun passwordCheckRegex(passwordCheck: String): Boolean =
+        passwordCheck.matches("^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[\$@\$!%*#?&.])[A-Za-z[0-9]\$@\$!%*#?&.]{8,16}\$".toRegex())
+
+    private fun isInvalidEditTextPassword(): Boolean {
+        val etInsert = binding.etFindPasswordInsert.text.toString()
+        val etInsertMore =
+            binding.etFindPasswordInsertMore.text.toString()
+
+        if (etInsertMore != "" && etInsertMore != etInsert) return false
+        return true
+    }
+
+    private fun checkPassword(password: String) {
+        when {
+            password.isEmpty() -> {
+                binding.etFindPasswordInsert.error =
+                    resources.getString(R.string.sign_up_check_pw)
+                passwordFlag = false
+            }
+
+            !passwordRegex(password) -> {
+                binding.etFindPasswordInsert.error =
+                    resources.getString(R.string.sign_up_check_pw_regex)
+                passwordFlag = false
+            }
+
+            !passwordCheckRegex(password) -> {
+                binding.etFindPasswordInsertMore.error =
+                    resources.getString(R.string.sign_up_check_pw_regex)
+                passwordCheckFlag = false
+            }
+
+            password.isNotEmpty() -> {
+                binding.etFindPasswordInsert.error = null
+                passwordFlag = true
+
+                if (!isInvalidEditTextPassword()) {
+                    binding.etFindPasswordInsertMore.error =
+                        resources.getString(R.string.sign_up_check_pw_not)
+                    passwordCheckFlag = false
+                    passwordFlag = true
+                } else {
+                    binding.etFindPasswordInsertMore.error = null
+                    passwordCheckFlag = true
+                }
+            }
+        }
+    }
+
+    private fun checkPasswordAgain(password: String) {
+        when {
+            password.isEmpty() -> {
+                binding.etFindPasswordInsertMore.error =
+                    resources.getString(R.string.sign_up_check_pw)
+                passwordFlag = false
+            }
+
+            !passwordCheckRegex(password) -> {
+                binding.etFindPasswordInsertMore.error =
+                    resources.getString(R.string.sign_up_check_pw_regex)
+                passwordCheckFlag = false
+            }
+
+            password.isNotEmpty() -> {
+                binding.etFindPasswordInsertMore.error = null
+                passwordFlag = true
+                when {
+                    binding.etFindPasswordInsertMore.text.toString() != ""
+                            && binding.etFindPasswordInsertMore.text.toString() != binding.etFindPasswordInsert.text.toString() -> {
+                        binding.etFindPasswordInsertMore.error =
+                            resources.getString(R.string.sign_up_check_pw_not)
+                        passwordCheckFlag = false
+                        passwordFlag = true
+                    }
+
+                    else -> {
+                        binding.etFindPasswordInsertMore.error = null
+                        passwordCheckFlag = true
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/org/cazait/ui/findaccount/findpassword/FindUserPasswordViewModel.kt
+++ app/src/main/java/org/cazait/ui/findaccount/findpassword/FindUserPasswordViewModel.kt
@@ -1,9 +1,26 @@
 package org.cazait.ui.findaccount.findpassword
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
+import org.cazait.model.Resource
+import org.cazait.model.UserPassword
 import org.cazait.ui.base.BaseViewModel
+import org.cazait.utils.SingleEvent
 import javax.inject.Inject
 
 @HiltViewModel
 class FindUserPasswordViewModel @Inject constructor() : BaseViewModel() {
+    private val _changePasswordProcess = MutableLiveData<Resource<UserPassword>>()
+    val changePasswordProcess: LiveData<Resource<UserPassword>>
+        get() = _changePasswordProcess
+
+    private val _showToast = MutableLiveData<SingleEvent<Any>>()
+    val showToast: LiveData<SingleEvent<Any>>
+        get() = _showToast
+
+    fun showToastMessage(errorMessage: String?) {
+        if (errorMessage == null) return
+        _showToast.value = SingleEvent(errorMessage)
+    }
 }
