9d157c4b753dfb4643435714a9568adf86e4ce37
bmsk <vv99911@gmail.com>
2023-12-12 18:55:27 +0900

    [Feat] gradle 추가 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,11 +34,6 @@ dependencies {
     implementation(projects.core.datastore)
     implementation(projects.core.domain)
 
-    // AndroidX
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.constraintlayout)
-
     // retrofit
     implementation(libs.retrofit)
     implementation(libs.converter.moshi)
@@ -51,23 +46,13 @@ dependencies {
     implementation(libs.androidx.legacy.support.v4)
     implementation(libs.androidx.databinding.runtime)
     implementation(libs.car.ui.lib)
-    implementation(libs.androidx.fragment.ktx)
-    implementation(libs.androidx.lifecycle.viewmodel.ktx)
-
-    // splash
-    implementation(libs.androidx.core.splashscreen)
 
     // room
     implementation(libs.androidx.room.ktx)
     implementation(libs.androidx.room.runtime)
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
     annotationProcessor(libs.androidx.room.compiler)
     testImplementation(libs.androidx.room.testing)
     kapt(libs.androidx.room.compiler)
-
-    // Preferences DataStore
-    implementation(libs.androidx.datastore.preferences)
 
     // viewpager2
     implementation(libs.androidx.viewpager2)
@@ -76,11 +61,6 @@ dependencies {
 
     // expandable layout
     implementation(libs.expandablelayout)
-
-    // hilt
-    implementation(libs.hilt.android)
-    kapt(libs.hilt.android.compiler)
-    kaptAndroidTest(libs.hilt.android.compiler)
 
     // naver maps
     implementation(libs.map.sdk)
@@ -91,22 +71,13 @@ dependencies {
     // swipe refresh
     implementation(libs.androidx.swiperefreshlayout)
 
-    // Moshi
-    implementation(libs.moshi)
-    // Gson
-    implementation(libs.converter.gson)
-
     // Dot Indicator
     implementation(libs.dotsindicator)
 
     // Glide
     implementation(libs.glide)
     annotationProcessor(libs.compiler)
-
     implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-
     // coil
     implementation(libs.coil)
 
